e3ecf62d9e140d79bfefa96d26bdd5db1139d008
Alex7k <51133872+Alex7k@users.noreply.github.com>
2025-12-04 17:25:57 +0000

    dynamically build sections from sections.json

--- src/services.js
+++ src/services.js
@@ -1,30 +1,38 @@
-const host = window.location.hostname;
-const protocol = window.location.protocol;
+async function loadServices() {
+  const response = await fetch("sections.json");
+  const data = await response.json();
 
-const media = {
-  Jellyfin: 8096,
-  Jellyseer: 5055,
-  Sonarr: 8989,
-  Radarr: 7878,
-  Prowlarr: 9696,
-  qBitTorrent: 8080,
-};
+  const container = document.getElementById("container");
+  const host = window.location.hostname;
+  const protocol = window.location.protocol;
 
-const sensors = {
-  Exporter: "9100/metrics",
-  Prometheus: 9090,
-  Grafana: 3000,
-};
+  // page title
+  const h1 = document.createElement("h1");
+  // h1.textContent = `${protocol}//${host}`; // include protocol
+  h1.textContent = host;
+  container.appendChild(h1);
 
-function populate(id, items) {
-  const ul = document.getElementById(id);
-  for (const [name, port] of Object.entries(items)) {
-    const url = `${protocol}//${host}:${port}`;
-    const li = document.createElement("li");
-    li.innerHTML = `<a href="${url}">${name}</a>`;
-    ul.appendChild(li);
+  // build all sections
+  for (const [category, items] of Object.entries(data)) {
+
+    // section title
+    const h2 = document.createElement("h2");
+    h2.textContent = category;
+    container.appendChild(h2);
+
+    // create list element
+    const ul = document.createElement("ul");
+
+    // fill section items
+    for (const [name, dest] of Object.entries(items)) {
+      const li = document.createElement("li");
+      const url = `${protocol}//${host}:${dest[1]}`;
+      li.innerHTML = `<a href="${url}">${name}</a>`;
+      ul.appendChild(li);
+    }
+
+    container.appendChild(ul);
   }
 }
 
-populate("media", media);
-populate("sensors", sensors);
+loadServices();
